203f7d11e0f6179e0aa71b7df370dc28a6834541
balll <finch255@hotmail.com>
2023-03-11 12:47:59 +0700

    init

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
+"use strict";
+
 const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
   res.send("hello iqair");
 });
-
 app.listen(3100, function () {
   console.log("start :host");
 });
